8c4480234652c03430118e3b0ed2327ef12dada1
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-02-17 19:40:01 +0530

    Create ExcelToPython.py

---
+++ classes/17_feb/ExcelToPython.py
@@ -0,0 +1,49 @@
+import pandas
+import sqlite3
+
+
+if __name__=="__main__":
+  
+
+    # read excel data and convert it to python dict
+    
+    excel_data= pandas.read_excel('hospitaldata.xlsx')
+    dict_data= excel_data.to_dict()
+    
+    
+    # connect database (database name: Hospital_DB) 
+    
+    db = sqlite3.connect('Hospital_DB.sqlite')
+    obj_cur = db.cursor()
+    
+    
+    # loop to excecute insertion queries for db
+    
+    print("------------------start----------------------")
+
+    for i in range(len(dict_data['DAYS'])):
+      
+        # table BEDS have 4 collumns HOSPITAL_ID, DISEASE, DAYS_BED_AVAILIBILITY and one collomn for patient id which is set to autoincreament mood
+        
+        db_query= f'''INSERT INTO BEDS(
+            HOSPITAL_ID, DISEASE, DAYS_BED_AVAILIBILITY
+            ) 
+            VALUES(
+                {int(dict_data['HOSPITAL_ID'][i])},
+                "{dict_data['DISEASE'][i]}",
+                {int(dict_data['DAYS'][i])}
+        );'''
+        print("------------------data stored----------------------",i)
+        
+        # excecute above query and save it to db
+        
+        obj_cur.execute(db_query)
+        db.commit()
+
+    db.close()
+    print("------------------end----------------------")
+
+
+    
+
+
