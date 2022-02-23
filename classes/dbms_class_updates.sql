28cac5e7e4396468fc1be7e866c0243f440ff17b
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-02-23 15:10:44 +0530

    Update dbms_class_updates.sql

--- classes/dbms_class_updates.sql
+++ classes/dbms_class_updates.sql
@@ -10,9 +10,13 @@ CREATE TABLE BEDS(
     DISEASE VARCHAR(40),
     DAYS_BED_AVAILIBILITY INTEGER
                  );
-                 
+
+INSERT INTO BEDS VALUES(21002,10001,fever,2),(21006,10002,stroke,2);
+
 # Records were inserted from excel to BEDS
 # https://docs.google.com/spreadsheets/d/18mcakEtf8PICwVpuExcfL6W6Ic5EVStZfoPGW1gRRzA/edit?usp=sharing
+# 
+
 
 SELECT *FROM BEDS;
 
@@ -23,8 +27,16 @@ CREATE TABLE PATIENT(
     DIABETES VARCHAR(40),
     AREA_PIN_CODE INTEGER
                     );
-                    
+ 
+ INSERT INTO PATIENT VALUES(1,Pranjal,A+,Y,102),(2,shiva,B-,N,102);
+ 
  # Records were inserted from excel to PATIENT
  # https://docs.google.com/spreadsheets/d/1VPw1Lb-eq7CnUFpvjMRzkVhJ2iKL6dN3b1kBhSt2p58/edit?usp=sharing
+ # 
 
 SELECT *FROM PATIENT;
+
+SELECT * FROM BEDS INNER JOIN PATIENT;
+
+UPDATE PATIENT SET S_NO = S_NO+10000;
+
