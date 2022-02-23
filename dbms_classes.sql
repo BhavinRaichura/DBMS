18cb430b4766acd0526b8d652f4326e6b0e508fb
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-02-23 14:56:20 +0530

    Update dbms_classes.sql

--- dbms_classes.sql
+++ dbms_classes.sql
@@ -1,17 +1,20 @@
 SHOW DATABASES ;
-# CREATE DATABASE HOSPITAL_BED_AVAILIBILITY;
-# USE HOSPITAL_BED_AVAILIBILITY;
+
+CREATE DATABASE HOSPITAL_BED_AVAILIBILITY;
+USE HOSPITAL_BED_AVAILIBILITY;
 SHOW TABLES;
 
-/*
 CREATE TABLE BEDS(
     HOSPITAL_ID INTEGER,
     PATIENT_ID INTEGER PRIMARY KEY,
     DISEASE VARCHAR(40),
     DAYS_BED_AVAILIBILITY INTEGER
                  );
-*/
+                 
+# Records were inserted from excel to BEDS
+# https://docs.google.com/spreadsheets/d/18mcakEtf8PICwVpuExcfL6W6Ic5EVStZfoPGW1gRRzA/edit#gid=0
 
+SELECT *FROM BEDS;
 
 CREATE TABLE PATIENT(
     S_NO INTEGER PRIMARY KEY,
@@ -20,6 +23,9 @@ CREATE TABLE PATIENT(
     DIABETES VARCHAR(40),
     AREA_PIN_CODE INTEGER
                     );
+                    
+ # Records were inserted from excel to PATIENT
+ # https://docs.google.com/spreadsheets/d/1VPw1Lb-eq7CnUFpvjMRzkVhJ2iKL6dN3b1kBhSt2p58/edit#gid=0
 
 SELECT *FROM PATIENT;
 
