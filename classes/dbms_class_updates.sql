4922b6f2c404384927515d26f051117374434509
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-02-23 15:26:14 +0530

    Update dbms_class_updates.sql

--- classes/dbms_class_updates.sql
+++ classes/dbms_class_updates.sql
@@ -1,9 +1,11 @@
-SHOW DATABASES ;
+##-- 17 feb
 
+SHOW DATABASES ;
 CREATE DATABASE HOSPITAL_BED_AVAILIBILITY;
 USE HOSPITAL_BED_AVAILIBILITY;
 SHOW TABLES;
 
+##-- 18 feb
 CREATE TABLE BEDS(
     HOSPITAL_ID INTEGER,
     PATIENT_ID INTEGER PRIMARY KEY,
@@ -17,7 +19,7 @@ INSERT INTO BEDS VALUES(21002,10001,'fever',2),(21006,10002,'stroke',2);
 # https://docs.google.com/spreadsheets/d/18mcakEtf8PICwVpuExcfL6W6Ic5EVStZfoPGW1gRRzA/edit?usp=sharing
 # 
 
-
+##-- 22 feb
 SELECT *FROM BEDS;
 
 CREATE TABLE PATIENT(
@@ -27,13 +29,13 @@ CREATE TABLE PATIENT(
     DIABETES VARCHAR(40),
     AREA_PIN_CODE INTEGER
                     );
+                    
+##-- 23 feb
+INSERT INTO PATIENT VALUES(1,'Pranjal','A+','Y',102),(2,'shiva','B-','N',102);
  
- INSERT INTO PATIENT VALUES(1,'Pranjal','A+','Y',102),(2,'shiva','B-','N',102);
- 
- # Records were inserted from excel to PATIENT
- # https://docs.google.com/spreadsheets/d/1VPw1Lb-eq7CnUFpvjMRzkVhJ2iKL6dN3b1kBhSt2p58/edit?usp=sharing
- # 
-
+# Records were inserted from excel to PATIENT
+# https://docs.google.com/spreadsheets/d/1VPw1Lb-eq7CnUFpvjMRzkVhJ2iKL6dN3b1kBhSt2p58/edit?usp=sharing
+# 
 SELECT *FROM PATIENT;
 
 SELECT * FROM BEDS INNER JOIN PATIENT;
