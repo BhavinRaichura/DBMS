98ee8f72a6e05596958e5c8f6b2b594ef4c38dcf
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-02-26 00:02:06 +0530

    Update dbms_class_updates.sql

--- classes/dbms_class_updates.sql
+++ classes/dbms_class_updates.sql
@@ -1,11 +1,13 @@
-##-- 17 feb
+-- 17 feb
 
-SHOW DATABASES ;
+SHOW DATABASES;
+
 CREATE DATABASE HOSPITAL_BED_AVAILIBILITY;
 USE HOSPITAL_BED_AVAILIBILITY;
 SHOW TABLES;
 
-##-- 18 feb
+-- 18 feb
+
 CREATE TABLE BEDS(
     HOSPITAL_ID INTEGER,
     PATIENT_ID INTEGER PRIMARY KEY,
@@ -15,11 +17,11 @@ CREATE TABLE BEDS(
 
 INSERT INTO BEDS VALUES(21002,10001,'fever',2),(21006,10002,'stroke',2);
 
-# Records were inserted from excel to BEDS
-# https://docs.google.com/spreadsheets/d/18mcakEtf8PICwVpuExcfL6W6Ic5EVStZfoPGW1gRRzA/edit?usp=sharing
-# 
+-- Records were inserted from excel to BEDS
+-- https://docs.google.com/spreadsheets/d/18mcakEtf8PICwVpuExcfL6W6Ic5EVStZfoPGW1gRRzA/edit?usp=sharing
+ 
+-- 22 feb
 
-##-- 22 feb
 SELECT *FROM BEDS;
 
 CREATE TABLE PATIENT(
@@ -30,18 +32,20 @@ CREATE TABLE PATIENT(
     AREA_PIN_CODE INTEGER
                     );
                     
-##-- 23 feb
+-- 23 feb
+
 INSERT INTO PATIENT VALUES(1,'Pranjal','A+','Y',102),(2,'shiva','B-','N',102);
  
-# Records were inserted from excel to PATIENT
-# https://docs.google.com/spreadsheets/d/1VPw1Lb-eq7CnUFpvjMRzkVhJ2iKL6dN3b1kBhSt2p58/edit?usp=sharing
-# 
+-- Records were inserted from excel to PATIENT
+-- https://docs.google.com/spreadsheets/d/1VPw1Lb-eq7CnUFpvjMRzkVhJ2iKL6dN3b1kBhSt2p58/edit?usp=sharing
+-- 
 SELECT *FROM PATIENT;
 
 SELECT * FROM BEDS INNER JOIN PATIENT;
 
 
-##-- 25 FEB
+-- 25 FEB
+
 UPDATE PATIENT SET S_NO = S_NO+10000;
 
 SELECT * FROM BEDS LEFT JOIN PATIENT ON BEDS.PATIENT_ID = PATIENT.S_NO;
@@ -50,7 +54,13 @@ SELECT * FROM BEDS RIGHT JOIN PATIENT ON BEDS.PATIENT_ID = PATIENT.S_NO;
 
 SELECT * FROM PATIENT RIGHT JOIN BEDS ON PATIENT.S_NO = BEDS.PATIENT_ID ;
 
+-- create new table using the output of join of two tables
 CREATE TABLE BEDS_AND_PATIENT AS SELECT * FROM PATIENT RIGHT JOIN BEDS ON PATIENT.S_NO = BEDS.PATIENT_ID ;
 
 SELECT * FROM BEDS_AND_PATIENT;
+
+
+
+
+
 
